920d5ed0ce3bf8b277a704bdccbd7f49d643739f
martijnkobus <martijnkobus@gmail.com>
2022-02-22 21:36:34 +0100

    Datatabel voorbeeld werkend

--- Data tabellen/restauranten_app.R
+++ Data tabellen/restauranten_app.R
@@ -8,9 +8,7 @@ library(shiny)
 
 
 ui <- fluidPage(
-#theme = bs_theme(version = 4, bootswatch = "flatly"),
-#titlePanel("Print van Datatable output"),
-  
+
   sidebarLayout(
     sidebarPanel(
       tags$h4("Ruwe output DT"),
@@ -30,7 +28,6 @@ ui <- fluidPage(
 )
 
 
-
 server <- function(input, output, session) {
   import <- read.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQWZjhKquH99PfeHq9I2dYwRvVuIeK1igjfEXuY5F50yJiSg5IenfQAwQuFy42vaji6vc8KI0Ta6jjT/pub?gid=0&single=true&output=csv')
   
@@ -39,14 +36,15 @@ server <- function(input, output, session) {
     mutate(popupLabel = paste0("<b>", Restaurant.naam,"</b>", "<br/>","</b>", "<br/>",
            paste0("Aanbevolen door:  <u>", Naam, "</u>")))
 
-  voorDataTabel <- import[,1:4]
-  names(voorDataTabel) <- c("collega naam", "restaurant naam", "lat", "lon")
+  restaurantsDataTabel <- import[,1:4]
+  names(restaurantsDataTabel) <- c("collega naam", "restaurant naam", "lat", "lon")
   
   output$dt_restaurants <- DT::renderDataTable({
-    
-    datatable(voorDataTabel,
-              selection = "multiple"
-    )
+    datatable(restaurantsDataTabel,
+              selection = "multiple",
+              class = 'cell-border stripe',
+              filter = 'top',
+              )
   })
   
   output$txt_out <- renderPrint({
@@ -63,8 +61,15 @@ server <- function(input, output, session) {
   visRestaurants <- import %>%
     filter(Vlees.vis.vega == "Vis")
 
+  dt_rows_selected <- reactive({
+    input$dt_restaurants_rows_selected
+  })
+
+  
   output$restaurantsKaartje <- renderLeaflet({
-  leaflet() %>%
+    i_rows <- dt_rows_selected()
+    
+    restaurantsKaartje <- leaflet() %>%
       addTiles(group = "OSM") %>%
       addProviderTiles("CartoDB", group = "Carto") %>%
       addProviderTiles("Esri.WorldImagery", group = "Esri") %>%
@@ -95,12 +100,23 @@ server <- function(input, output, session) {
         opacity = 1) %>%
       addLayersControl(baseGroups = c("Carto","OSM", "Esri", "Stamen"),
                        overlayGroups = c("Vegetarisch", "Vlees", "Vis"))
+  
+    if(!is.null(i_rows)){
+
+      restaurantsKaartje <- restaurantsKaartje %>%
+        addCircleMarkers(
+          lat = restaurantsDataTabel$lat[i_rows],
+          lng = restaurantsDataTabel$lon[i_rows],
+          popup = restaurantsDataTabel$popupLabel[i_rows],
+          color = "orange",
+          radius = 2,
+          opacity = 1)
+
+    }
+
+    restaurantsKaartje
+  
   })
 }
 
 shinyApp(ui, server)
-
-
-
-
-
